dfb06ab5327b330eb658810e0a4ca089198c26de
Ron Liebregts <ronlieb@hotmail.com>
2022-05-09 16:21:10 +0200

    Sorting by and time axis

--- front-end/src/app/dosing-details/dosing-details.component.ts
+++ front-end/src/app/dosing-details/dosing-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Logging } from '../models/Logging';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { CheckboxItem } from '../models/CheckboxItem';
 
 @Component({
   selector: 'app-dosing-details',
@@ -12,7 +13,8 @@ export class DosingDetailsComponent implements OnInit {
 
   loggings: Logging[] = [];
   loggingProps: String[] = [];
-  selectedItems: string[] = [];
+  selectedItems: String[] = [];
+  checkboxList: Array<any> = [];
   dropdownSettings = {};
   typeofdosing!: string;
   setpoint!: number | null;
@@ -30,6 +32,7 @@ export class DosingDetailsComponent implements OnInit {
   filterPlant?: string;
   filterDateFrom?: Date;
   filterDateUntil?: Date;
+  timeArray?: Array<number>;
 
  
   constructor(private dataService: DataService, private modalService: NgbModal) { }
@@ -50,7 +53,7 @@ export class DosingDetailsComponent implements OnInit {
     };
   }
 
-  onChangeCustomer(event?: any){
+  public onChangeCustomer(event?: any){
     if(this.filterCustomer != null && this.filterPlant == null && this.filterDateFrom == null && this.filterDateUntil == null)
     {
       //Query get customer information
@@ -58,7 +61,7 @@ export class DosingDetailsComponent implements OnInit {
     this.filterCustomer = event;
   }
 
-  onChangePlant(event?: any){
+  public onChangePlant(event?: any){
     this.filterPlant = event;
     if(this.filterPlant != null && this.filterDateFrom == null && this.filterDateUntil == null)
     {
@@ -66,6 +69,48 @@ export class DosingDetailsComponent implements OnInit {
     }
   }
   
+  public checkboxClick(prop: CheckboxItem){
+
+    if(prop.isChecked == false){
+      this.selectedItems.push(prop.name);
+      this.setOptions();
+      this.checkboxListUpdate();
+    }
+    if(prop.isChecked == true){
+      this.selectedItems.forEach((element,index)=>{
+        if(element == prop.name) this.selectedItems.splice(index,1);
+     });
+     this.setOptions();
+     this.checkboxListUpdate();
+    }
+
+    console.log(this.selectedItems);
+  }
+
+  public checkboxListUpdate(){
+    this.checkboxList = [];
+    for (var i = 0; i < this.loggingProps.length; i++) {
+
+      var ismatch = false; // we haven't found it yet
+
+      for (var j = 0; j < this.selectedItems.length; j++) {
+
+        if (this.loggingProps[i] == this.selectedItems[j]) {
+          // we have found this.loggingsProps[i]] in this.selectedItems, so we can stop searching
+          ismatch = true;
+          const checkitem: CheckboxItem = {name: this.loggingProps[i], isChecked: true};
+          this.checkboxList.push(checkitem);
+          break;
+        }//End if
+        // and ismatch will remain false
+      }
+      // add this.loggingsProps[i] to newArray only if we didn't find a match.
+      if (!ismatch) {
+        const checkitem: CheckboxItem = {name: this.loggingProps[i], isChecked: false};
+        this.checkboxList.push(checkitem);
+      } //End if
+    }
+  }
 
   public async onChangeID() {
     this.getLoading();
@@ -87,27 +132,49 @@ export class DosingDetailsComponent implements OnInit {
         this.setpoint = this.loggings[0].ifSetpoint;
         this.accuracy = this.loggings[0].ifAccuracy;
         this.dosedweight = this.loggings[this.loggings.length - 1].ifDosedWeight;
+        this.calculateTimeToSecond();
+        this.mapDosingTime();
         this.calculateMargins();
+        this.checkboxListUpdate();
         this.setOptions();
       });
   }
 
-  onItemSelect(item: any) {
-    console.log(this.selectedItems);
-    this.setOptions();
-  }
-  onSelectAll(items: any) {
-    console.log(this.selectedItems);
-    this.setOptions();
-  }
-  onItemDeSelect(item: any) {
-    console.log(this.selectedItems);
-    this.setOptions();
-  }
-
-  calculateMargins(){
+  public calculateMargins(){
     this.setpointMax = this.setpoint! + ((this.setpoint! / 100)  * this.accuracy!);
     this.setpointMin = this.setpoint! - ((this.setpoint! / 100)* this.accuracy!);
+  }
+
+  public calculateTimeToSecond(): number{
+    //7: micro, 6: seconden, 5: minuten, 4: uren, 3: dagen, 2: maanden, 1: jaren
+    let microseconds = this.loggings[this.loggings.length - 1].ifDosedTime7;
+    let seconds = this.loggings[this.loggings.length - 1].ifDosedTime6;
+    let minutes = this.loggings[this.loggings.length - 1].ifDosedTime5;
+    let hours = this.loggings[this.loggings.length - 1].ifDosedTime4;
+    let days = this.loggings[this.loggings.length - 1].ifDosedTime3;
+    let months = this.loggings[this.loggings.length - 1].ifDosedTime2;
+    let years = this.loggings[this.loggings.length - 1].ifDosedTime1;
+    
+    let covertedtoseconds = (microseconds!/1000000) + seconds! + (minutes!*60) + (hours!*3600) + (days!*86400) + (months!*2629743) + (years!*31556926);
+
+    return covertedtoseconds;
+  }
+
+  public mapDosingTime(){
+    let total = this.calculateTimeToSecond();
+    let steps = this.loggings.length - 1;
+    let increase = total / steps;
+
+    console.log(total);
+    console.log(steps);
+    console.log(increase);
+
+    this.timeArray = [0];
+
+    for(var i = 0; i <= steps; i++){
+      this.timeArray.push(Math.round((this.timeArray[this.timeArray.length - 1] + increase) * 100) / 100);
+    }
+    console.log(this.timeArray);
   }
 
   setOptions() {
@@ -143,7 +210,8 @@ export class DosingDetailsComponent implements OnInit {
       ],
       xAxis: {
         type: 'category',
-        data: this.loggings.map(c => c.ifDosedTime7)
+        // data: this.loggings.map(c => c.ifDosedTime7)
+        data: this.timeArray
       },
       yAxis: [
         {
---
+++ front-end/src/app/models/CheckboxItem.ts
@@ -0,0 +1,5 @@
+export interface CheckboxItem {
+    name: String
+    isChecked: boolean;
+
+}
